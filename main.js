bdf3069df444fad0dbfda20f7cc8f733bb9462b7
Vikelduk <108426561+Vikelduk@users.noreply.github.com>
2023-02-24 17:06:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,75 @@
+pontua = 0;
+
+document.getElementById("pontos").innerHTML = 'Pontuação: ' + pontua;
+
 array['lapis','celular','caderno','copo','aviao'];
 random = Math.floor((Math.random() * array.lenght)+1)
 
-document.getElementById("desafio").innerHTML = '<p id="desafio">Esboço a Ser Desenhado: '+random+' </p>'
-function setup()
-{
-    canvas = createCanvas(280, 280);
-    canvas.center();
-}
+document.getElementById("desafio").innerHTML = 'Esboço a Ser Desenhado: ' + random;
 
 setTimeout(
-    function()
+  function()
+  {
+    pontos();
+  }, 10000);
+
+document.getElementById("tempo").innerHTML = setTimeout(function(){pontos();},10000);
+
+  function preload()
+  {
+      classifier = ml5.imageClassifier('DoodleNet');
+  }
+  
+  function setup()
+  {
+      canvas = createCanvas(280, 280);
+      canvas.center();
+      background("white");
+      canvas.mouseReleased(classifyCanvas);
+  }
+  
+  function draw()
+  {
+      strokeWeight(13);
+      stroke(0);
+  
+      if (mouseIsPressed)
+      {
+          line(pmouseX, pmouseY, mouseX, mouseY); 
+      }
+  }
+  
+  function clearCanvas()
+  {
+      background("white");
+  }
+  
+  function classifyCanvas()
+  {
+      classifier.classify(canvas, gotResults);
+  }
+  
+  function gotResults(error, results)
+  {
+      if (error)
+      {
+          console.error(error);
+      }
+      console.log(results);
+      var result = results[0].label;
+      document.getElementById("label").innerHTML = 'Nome: ' + result.replace('_', ' ');
+      
+      confidencia = Math.round(results[0].confidence * 100)
+
+      document.getElementById("confidence").innerHTML = 'Precisão: ' + confidencia + '%';
+      pontos();
+      
+  }
+
+  function pontos()
+  {
+    if (confidencia > 60 )
     {
-      takeSelfie1();
-      save1();
-    }, 10000);
+      pontua = pontua + 1; 
+    }
+  }
